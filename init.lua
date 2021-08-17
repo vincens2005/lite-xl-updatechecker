ae109c237248eb732421eff79794f468c5d25afd
cukmekerb <cukmekerb@gmail.com>
2021-08-17 15:38:58 -0700

    use new process API

--- init.lua
+++ init.lua
@@ -9,19 +9,15 @@ config.updatechecker_timeout = 3 -- increase this value if you get json.lua erro
 
 -- stolen from git status
 local function exec(cmd, wait)
-	local tempfile = core.temp_filename()
-	system.exec(string.format("%s > %q", cmd, tempfile))
+	local proc = process.start(cmd, {timeout = wait})
 	coroutine.yield(wait)
-	local fp = io.open(tempfile)
-	local res = fp:read("*a")
-	fp:close()
-	os.remove(tempfile)
+	proc:wait(wait)
+	local res = proc:read_stdout(4096 * 10)
 	return res
 end
 
 local function fetch(url)
-	local cmd = "curl "
-	cmd = cmd .. url
+	local cmd = {"curl", url}
 	local result = exec(cmd, config.updatechecker_timeout)
 	return result
 end
@@ -50,14 +46,14 @@ local function check_updates()
 	core.log_quiet("checking for updates...")
 
 	core.add_thread(function()
-		local raw_data = fetch("https://api.github.com/repos/lite-xl/lite-xl/releases")
+		local raw_data = fetch("https://api.github.com/repos/lite-xl/lite-xl/releases/latest")
 		local data = json.decode(raw_data)
 
-		core.log_quiet(data[1].tag_name)
+		core.log_quiet(data.tag_name)
 
 		local current_version = "v" .. VERSION
 
-		if current_version == data[1].tag_name or data[1].draft or data[1].prerelease then
+		if current_version == data.tag_name or data.draft or data.prerelease then
 			core.log_quiet("lite-xl is up to date")
 			return
 		end
@@ -68,7 +64,7 @@ local function check_updates()
 		}
 
 		core.nag_view:show("new update available",
-		                   "lite xl " .. data[1].tag_name .. " is ready to download", opt, function(item)
+		                   "lite xl " .. data.tag_name .. " is ready to download", opt, function(item)
 
 			if item.text == "download" then
 				core.add_thread(function()
@@ -76,13 +72,13 @@ local function check_updates()
 
 					-- if on windows, just open release page, since multiple windows binaries exist
 					if os_name == "windows" then
-						system.exec("start " .. data[1].html_url)
+						system.exec("start " .. data.html_url)
 						core.log("opening in browser...")
 						return
 					end
 
 					if os_name == "linux" then
-						local download_item = arr_find(data[1].assets, function(asset)
+						local download_item = arr_find(data.assets, function(asset)
 							return asset.browser_download_url:find("linux")
 						end)
 						core.log("opening in browser...")
@@ -91,7 +87,7 @@ local function check_updates()
 					end
 
 					if os_name == "macos" then
-						local download_item = arr_find(data[1].assets, function(asset)
+						local download_item = arr_find(data.assets, function(asset)
 							return asset.browser_download_url:find("macos")
 						end)
 						system.exec("open " .. download_item.browser_download_url)
